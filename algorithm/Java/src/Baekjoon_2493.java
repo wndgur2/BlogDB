271eee182fc5736953fd7a818776f15be3884749
leejunghyeok <wndgur76@naver.com>
2024-07-11 22:33:52 +0900

    2493 풀이

--- algorithm/Java/src/Baekjoon_2493.java
+++
@@ -1,8 +0,0 @@
-public class Baekjoon_2493 {
-    public static void main(String[] args) {
-        System.out.println(Baekjoon_2493.solve());
-    }
-    static int solve(){
-        return 1;
-    }
-}
