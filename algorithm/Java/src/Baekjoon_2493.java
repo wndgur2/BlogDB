0f02c0d1a49a3c7a93aa2c0b319368b430e89311
leejunghyeok <wndgur76@naver.com>
2024-07-11 21:16:16 +0900

    test

---
+++ algorithm/Java/src/Baekjoon_2493.java
@@ -0,0 +1,8 @@
+public class Baekjoon_2493 {
+    public static void main(String[] args) {
+        System.out.println(Baekjoon_2493.solve());
+    }
+    static int solve(){
+        return 1;
+    }
+}
